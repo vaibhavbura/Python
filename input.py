189f92408efb899110347ed4ed612cf608333215
Vaibhav Bura <140695844+vaibhavbura@users.noreply.github.com>
2024-01-14 00:42:40 +0530

    mod

---
+++ input.py
@@ -0,0 +1,3 @@
+name = input("Enter your name:")
+age = int(input("Enter your age:"))
+print("Your name is ",name, "you are ",age ,"old")
---
+++ list.py
@@ -0,0 +1,14 @@
+countries = ['United Kingdom','India','Africa','Nigeria']
+
+print(type(countries))
+print(countries[1:2])
+
+list1 = [5,4,3,2,1]
+list2 = ['bannana','apple','mango','oranges']
+list2.append('cheery')
+#list1.extend(list2)
+list2.reverse()
+print(list2)
+list1.sort()
+print(list2.index('apple'))
+print(list1)
